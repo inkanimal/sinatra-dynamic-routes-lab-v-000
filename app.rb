94e6368a6ef3e9630d15d4f1b5a564b938ae7a83
geoffrey woodcock <theatomproject@gmail.com>
2018-07-30 18:37:34 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,10 +7,15 @@ class App < Sinatra::Base
     "#{@user_name}!"
    end
    
-   get "/square/:number" do
-    @number = params[:number].to_i.Math.sqrt
-    "#{@number}"
-   end
+  # get "/square/:number" do
+  #   @number = params[:number].to_i.Math.sqrt
+  #   "#{@number}"
+  # end
+   
+   get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number*@number}"
+  end
      
    get '/say/:number/:phrase' do
     @number = params[:number].to_i
