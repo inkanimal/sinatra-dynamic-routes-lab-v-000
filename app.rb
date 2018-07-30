a0c47a4350bce5a02c61c975442187f18a9e77bb
geoffrey woodcock <theatomproject@gmail.com>
2018-07-30 18:32:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,8 +13,8 @@ class App < Sinatra::Base
    end
    
    get "say/:number/:phrase" do 
-     @num = params[:number].to_i 
-     @phrase = params[:phrase]
+     num = params[:number].to_i 
+     phrase = params[:phrase]
      
      
    end
