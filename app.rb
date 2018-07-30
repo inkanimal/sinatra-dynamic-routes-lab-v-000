82861d0f6f6b70e4cb880ef26449ece177673118
geoffrey woodcock <theatomproject@gmail.com>
2018-07-30 17:54:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ class App < Sinatra::Base
    end
    
    get "/square/:number" do
-    @number = params[:number].to
-    "#{@user_name}!"
+    @number = params[:number].to_i
+    "#{@number}!"
    end
    
 end
