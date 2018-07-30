2156358e215dc8cac42ba0dac7fed3751a9a0c8f
geoffrey woodcock <theatomproject@gmail.com>
2018-07-30 17:59:26 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
    end
    
    get "/square/:number" do
-    @number = params[:number].to_i.Math.sqrt.to_s
+    @number = params[:number].to_i.Math.sqrt
     "#{@number}"
    end
    
