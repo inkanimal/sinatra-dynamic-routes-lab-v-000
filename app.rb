629cd2a55a24d5f78fe51047d7ef606177e1f38f
geoffrey woodcock <theatomproject@gmail.com>
2018-07-30 18:11:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,10 +15,7 @@ class App < Sinatra::Base
    get "say/:number/:phrase" do 
      @num = params[:number].to_i 
      @phrase = params[:phrase]
-    def repeat(phrase=@phrase, c=@num)
-     c.times.collect { phrase }.join(' ')
-    end 
-    "#{repeat}"
+     erb: 
    end
    
 end
